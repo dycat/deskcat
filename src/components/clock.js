52d002b6113a10232c3c5da19fbc84bc5a6197cc
dyang504 <dengyang92@qq.com>
2020-08-02 20:44:37 +0800

    fix time count

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,26 +1,25 @@
 import React, { useState, useEffect } from "react";
 import "../static/css/clock.css";
 
-function formattedTime(showSeconds = false) {
-  let now = new Date();
+function formattedTime(now, showSeconds = false) {
   let hours = now.getHours() >= 10 ? now.getHours() : "0" + now.getHours();
   let minutes =
     now.getMinutes() >= 10 ? now.getMinutes() : "0" + now.getMinutes();
+  let seconds =
+    now.getSeconds() >= 10 ? now.getSeconds() : "0" + now.getSeconds();
   if (showSeconds) {
-    let seconds =
-      now.getSeconds() >= 10 ? now.getSeconds() : "0" + now.getSeconds();
     return hours + ":" + minutes + ":" + seconds;
   }
   return hours + ":" + minutes;
 }
 
 function Clock() {
-  let now = formattedTime(true);
+  let now = new Date();
   let [time, setTime] = useState(now);
 
   useEffect(() => {
     const updateTime = setTimeout(() => {
-      let now = formattedTime(true);
+      let now = new Date();
       setTime(now);
     }, 1000);
     return () => {
@@ -28,7 +27,7 @@ function Clock() {
     };
   }, [time]);
 
-  return <div className="clock">{time}</div>;
+  return <div className="clock">{formattedTime(time)}</div>;
 }
 
 export default Clock;
