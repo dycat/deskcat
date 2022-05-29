0f49fce1f12d553e9dbb079f8f5fac561c43fb6c
dycat <8856528+dycat@users.noreply.github.com>
2022-05-29 16:41:51 +0800

    fix configuretest

--- cypress/integration/configuretest.spec.ts
+++ cypress/integration/configuretest.spec.ts
@@ -1,6 +1,6 @@
 describe('test for configure funtions', () => {
     it('test for setup background', () => {
         cy.visit('http://localhost:3000')
-        cy.get('@configure_id').should('be.visible')
+        cy.get('#configure_id').click().should('be.visible')
     })
 })
